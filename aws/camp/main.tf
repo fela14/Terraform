2030f1a35a1f6b9a4570ee6662f99cc42e2674dd
fela14 <k.fakeye@yahoo.com>
2025-09-04 17:33:15 +0000

    aws

--- aws/camp/main.tf
+++ aws/camp/main.tf
@@ -9,10 +9,10 @@ resource "aws_vpc" "ntc_vpc" {
 }
 
 resource "aws_subnet" "ntc_public_subnet" {
-  vpc_id     = aws_vpc.ntc_vpc.id
-  cidr_block = "10.123.1.0/24"
+  vpc_id                  = aws_vpc.ntc_vpc.id
+  cidr_block              = "10.123.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone = "us-east-1a"
+  availability_zone       = "us-east-1a"
 
   tags = {
     Name = "dev"
@@ -20,17 +20,56 @@ resource "aws_subnet" "ntc_public_subnet" {
 }
 
 resource "aws_internet_gateway" "ntc_internet_gateway" {
-    vpc_id = aws_vpc.ntc_vpc.id
-  
+  vpc_id = aws_vpc.ntc_vpc.id
+
   tags = {
     Name = "dev"
   }
 }
 
 resource "aws_route_table" "vpc_public_rt" {
-    vpc_id = aws_vpc.ntc_vpc.id
-  
+  vpc_id = aws_vpc.ntc_vpc.id
+
   tags = {
     Name = "dev"
   }
 }
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.vpc_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.ntc_internet_gateway.id
+}
+
+resource "aws_route_table_association" "ntc_public_assoc" {
+  subnet_id      = aws_subnet.ntc_public_subnet.id
+  route_table_id = aws_route_table.vpc_public_rt.id
+} 
+
+resource "aws_security_group" "ntc_sg" {
+  name = "ntc_sg"
+  description = "dev security group"
+  vpc_id = aws_vpc.ntc_vpc.id
+
+  ingress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
+
+resource "aws_key_pair" "ntc_auth" {
+  key_name   = "ntc-key"
+  public_key = file("~/.ssh/ntc-key.pub")
+}
+
+
